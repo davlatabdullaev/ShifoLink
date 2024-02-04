bccee19329f4ba44d7151a8b96733c564eeaf89a
davlatabdullaev <davlatdavlat0206@gmail.com>
2024-02-05 02:26:11 +0500

    ShifoLink

---
+++ migration/postgres/001_create_tables.down.sql
@@ -0,0 +1,31 @@
+drop table if exists clinic;
+
+drop table if exists clinic_branch;
+
+drop table if exists doctor_type;
+
+drop table if exists clinic_admin;
+
+drop table if exists doctor;
+
+drop table if exists customer;
+
+drop table if exists queue;
+
+drop table if exists drug_store;
+
+drop table if exists drug_store_branch;
+
+drop table if exists drug;
+
+drop table if exists pharmacist;
+
+drop table if exists order;
+
+drop table if exists order_drug;
+
+drop table if exists author;
+
+drop table if exists journal;
+
+drop table if exists super_admin;
